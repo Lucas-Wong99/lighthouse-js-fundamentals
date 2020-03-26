a189b067aa064433271b819252635076bb67ca2a
Lucas Wong <lucaswong.ca@gmail.com>
2020-03-26 21:28:07 +0000

    First commit with practice on conditionals file.

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+const temp = -6;
+
+if (temp <= 0) {
+  console.log("I'm gunna need a scarf and coat.");
+} else if (temp < 15) {
+  console.log("A short sleeve won't cut it, go put on a light coat.");
+} else {
+  console.log("Good weather outside, I'm going to wear a t-shirt.");
+}
+
+console.log("You're ready to go outside!")
