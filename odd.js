803aa49b1f221e0fd9f23effbb606aaff33059f0
Lucas Wong <lucaswong.ca@gmail.com>
2020-03-31 23:39:59 +0000

    Added comments for my own sake

--- odd.js
+++ odd.js
@@ -1,3 +1,6 @@
+//A function that determines if a number is odd by taking an argument(num) and saying that if the number
+// divided by two does NOT equal 0 then it is true.
+
 const isOdd = function(num) {
   return num % 2 != 0
 }
